522dd4d91f43266b654c18c5ac4d39b24044da7c
Kingsley Godwin <65616098+realninety5@users.noreply.github.com>
2022-07-17 01:58:30 +0100

    Update

--- src/assignment.js
+++ src/assignment.js
@@ -9,14 +9,17 @@ function sumOfNumbers(arrayOfNumbers) {
     return arrayOfNumbers.reduce((a, b) => a+b, 0);
 }
 
+assignment.sumOfNumbers = sumOfNumbers;
 
-
+// 
 function evenOut(value) {
     return value % 2 == 0;
 }
 function countEvenNumbers(arrayOfNumbers) {
     return arrayOfNumbers.filter(evenOut).length;
 }
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 
 function celsiusToFahrenheit(arrayOfNumbers) {
@@ -26,6 +29,8 @@ function celsiusToFahrenheit(arrayOfNumbers) {
 function toF(value) {
     return Math.trunc((value * 9/5) + 32)
 }
+
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
